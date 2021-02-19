185ed811e4a258129236e4b3c4d92ceb19e6c656
Heiller Martinez <mjohnheiller@ymail.com>
2021-02-19 09:49:33 -0500

    Feat: Create first controllers in API

---
+++ CommunicationFiling/Controllers/ActionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunicationFiling.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Action = CommunicationFiling.DAL.Entities.Action;
+
+namespace CommunicationFiling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActionController : ControllerBase
+    {
+        public IConfiguration _configuration { get; }
+        private readonly CommFilingContext _context;
+
+        public ActionController(CommFilingContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+    }
+}
