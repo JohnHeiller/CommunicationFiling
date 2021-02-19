9bf8de5fee5296c88d5abf6ef6bda753bf3715e6
Heiller Martinez <mjohnheiller@ymail.com>
2021-02-19 09:49:00 -0500

    Feat: 006 - Create Automapper between DTO and Entity

---
+++ CommunicationFiling/DTO/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CommunicationFiling.DAL.Entities;
+
+namespace CommunicationFiling.DTO
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Action, ActionDTO>().ReverseMap();
+            CreateMap<Audit, AuditDTO>().ReverseMap();
+            CreateMap<CorrespondenceType, CorrespondenceTypeDTO>().ReverseMap();
+            CreateMap<Filing, FilingDTO>().ReverseMap();
+            CreateMap<RoleAction, RoleActionDTO>().ReverseMap();
+            CreateMap<Role, RoleDTO>().ReverseMap();
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<UserRole, UserRoleDTO>().ReverseMap();
+        }
+    }
+}
