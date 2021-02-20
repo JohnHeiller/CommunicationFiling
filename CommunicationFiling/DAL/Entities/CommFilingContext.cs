d451e3f3653b6933b58bbb673ecb9e5b1a7edaa7
Heiller Martinez <mjohnheiller@ymail.com>
2021-02-19 21:30:55 -0500

    Fix: 003 - Add OnModelCreating for DBContext

--- CommunicationFiling/DAL/Entities/CommFilingContext.cs
+++ CommunicationFiling/DAL/Entities/CommFilingContext.cs
@@ -21,5 +21,94 @@ namespace CommunicationFiling.DAL.Entities
         public DbSet<RoleAction> RolesActions { get; set; }
         public DbSet<CorrespondenceType> CorrespondenceTypes { get; set; }
         public DbSet<Filing> Filings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<User>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<UserRole>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<UserRole>().HasOne(o => o.Role)
+                .WithMany()
+                .HasForeignKey(s => s.RoleId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<UserRole>().HasOne(o => o.User)
+                .WithMany()
+                .HasForeignKey(s => s.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Audit>().HasOne(o => o.CreationUser)
+                .WithMany()
+                .HasForeignKey(s => s.CreationUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Audit>().HasOne(o => o.ModificationUser)
+                .WithMany()
+                .HasForeignKey(s => s.ModificationUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Action>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<CorrespondenceType>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Filing>().HasOne(o => o.AddresseeUser)
+                .WithMany()
+                .HasForeignKey(s => s.AddresseeUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Filing>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Filing>().HasOne(o => o.CorrespondenceType)
+                .WithMany()
+                .HasForeignKey(s => s.CorrespondenceTypeId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Filing>().HasOne(o => o.SenderUser)
+                .WithMany()
+                .HasForeignKey(s => s.SenderUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<RoleAction>().HasOne(o => o.Action)
+                .WithMany()
+                .HasForeignKey(s => s.ActionId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RoleAction>().HasOne(o => o.Role)
+                .WithMany()
+                .HasForeignKey(s => s.RoleId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<RoleAction>().HasOne(o => o.Audit)
+                .WithMany()
+                .HasForeignKey(s => s.AuditId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+        }
     }
 }
